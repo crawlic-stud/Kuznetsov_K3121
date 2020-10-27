fdad29730d484b8c66cd7ad783531cc9ac86ce49
crawlic-stud <nikitosik0726@gmail.com>
2020-10-27 16:39:45 +0300

    I did the Vigenere function

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -10,7 +10,26 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    import string
+    up = str(string.ascii_uppercase * 2)
+    low = str(string.ascii_lowercase * 2)
+    key = keyword
+    
+    if (len(keyword) < len(plaintext)):
+        while (len(key) < len(plaintext)):
+            key += keyword
+    
+    for i in range(len(plaintext)):    
+        key = key.upper()
+        shift = (string.ascii_uppercase).index(key[i])
+        
+        if plaintext[i].isupper():
+            ciphertext += up[up.index(plaintext[i])+shift]
+        elif plaintext[i].islower():
+            ciphertext += low[low.index(plaintext[i])+shift]
+        else:
+        	ciphertext += plaintext[i]
+    
     return ciphertext
 
 
@@ -26,5 +45,24 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    import string
+    up = str(string.ascii_uppercase * 2)
+    low = str(string.ascii_lowercase * 2)
+    key = keyword
+    
+    if (len(keyword) < len(ciphertext)):     #нахожу расширенный ключ, если такой нужен
+        while (len(key) < len(ciphertext)): 
+            key += keyword
+    
+    for i in range(len(ciphertext)): 
+        key = key.upper()
+        shift = (string.ascii_uppercase).index(key[i]) #нахожу сдвиг, чтобы использовать
+                                                       #код из предыдущего задания
+        if ciphertext[i].isupper():
+            plaintext += up[up.index(ciphertext[i])-shift]#использую свой код из Цезаря
+        elif ciphertext[i].islower():
+            plaintext += low[low.index(ciphertext[i])-shift]
+        else:
+        	plaintext += ciphertext[i]
+    
     return plaintext
