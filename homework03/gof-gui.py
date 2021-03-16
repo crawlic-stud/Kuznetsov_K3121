0918ad3e8d504189dd403154876aa0673713f4b5
crawlic-stud <nikitosik0726@gmail.com>
2021-03-16 19:39:01 +0300

    working gui version with console args

---
+++ homework03/gof-gui.py
@@ -0,0 +1,91 @@
+import argparse
+import pygame
+from pygame.locals import *
+
+from life import GameOfLife
+from ui import UI
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--width", help='Width of game screen', type=int, default=100)
+parser.add_argument("--height", help='Height of game screen', type=int, default=100)
+parser.add_argument("--cell_size", help='Size of one cell', type=int, default=10)
+parser.add_argument("--max_generations", help='Maximum amount of generations', type=int, default=100)
+
+args = parser.parse_args()
+
+class GUI(UI):
+
+    def __init__(self, life: GameOfLife, cell_size: int=10, speed: int=10) -> None:
+        super().__init__(life)
+        self.cell_size = cell_size
+        self.width = self.life.rows * self.cell_size
+        self.height = self.life.cols * self.cell_size
+        self.screen_size = self.width, self.height
+        self.screen = pygame.display.set_mode(self.screen_size)
+        self.cell_width = self.width // self.cell_size
+        self.cell_height = self.height // self.cell_size
+        self.speed = speed
+
+    def draw_lines(self) -> None:
+        for x in range(0, self.width, self.cell_size):
+            pygame.draw.line(self.screen, pygame.Color('black'),
+                             (x, 0), (x, self.height))
+        for y in range(0, self.height, self.cell_size):
+            pygame.draw.line(self.screen, pygame.Color('black'),
+                             (0, y), (self.width, y))
+
+    def draw_grid(self) -> None:
+        n = self.cell_size
+        for i in range(0, self.height, self.cell_size):
+            for j in range(0, self.width, self.cell_size):
+                point1 = i // n
+                point2 = j // n
+                if self.life.curr_generation[point1][point2]:
+                    pygame.draw.rect(self.screen, pygame.Color('black'), (j, i, n, n))
+                else:
+                    pygame.draw.rect(self.screen, pygame.Color('red'), (j, i, n, n))
+
+    def run(self) -> None:
+        pygame.init()
+        clock = pygame.time.Clock()
+        pygame.display.set_caption('Game of Life')
+        self.screen.fill(pygame.Color('white'))
+        self.life.create_grid(True)
+        pause = False
+        running = True
+        while running and self.life.is_changing and \
+                not self.life.is_max_generations_exceeded:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    running = False
+                elif event.type == KEYDOWN:
+                    if event.key == K_SPACE:
+                        pause = not pause
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    posx, posy = event.pos
+                    posx //= self.cell_size
+                    posy //= self.cell_size
+                    self.life.curr_generation[posy][posx] = \
+                        int(not bool(self.life.curr_generation[posy][posx]))
+                    self.draw_grid()
+                    pygame.display.flip()
+            if pause:
+                self.draw_grid()
+                self.draw_lines()
+                pygame.display.flip()
+                continue
+            self.draw_grid()
+            self.draw_lines()
+            self.life.step()
+            pygame.display.flip()
+            clock.tick(self.speed)
+        pygame.quit()
+
+
+
+if __name__ == '__main__':
+    print('УПРАВЛЕНИЕ:\nПРОБЕЛ - ПАУЗА\nЛКМ/ПКМ - ЗАКРАСИТЬ КЛЕТКУ')
+    ui = GUI((GameOfLife((args.width, args.height), True, max_generations=args.max_generations)), cell_size=args.cell_size)
+    ui.run()
