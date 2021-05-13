d51d56e9ca08e989f0d37d74b42b47bb66846a1d
crawlic-stud <nikitosik0726@gmail.com>
2021-05-13 13:59:17 +0300

    progress

--- homework04/pyvcs/repo.py
+++ homework04/pyvcs/repo.py
@@ -4,7 +4,7 @@ import typing as tp
 
 
 def repo_find(workdir: tp.Union[str, pathlib.Path] = ".") -> pathlib.Path:
-    git_dir = os.getenv("GIT_DIR", default=".pyvcs")
+    git_dir = os.getenv("GIT_DIR", default=".git")
     workdir = pathlib.Path(workdir)
     while pathlib.Path("/") != workdir.absolute():
         if (workdir / git_dir).is_dir():
@@ -17,12 +17,12 @@ def repo_find(workdir: tp.Union[str, pathlib.Path] = ".") -> pathlib.Path:
 
 
 def repo_create(workdir: tp.Union[str, pathlib.Path]) -> pathlib.Path:
-    git_dir = os.getenv("GIT_DIR", default=".pyvcs")
+    git_dir = os.getenv("GIT_DIR", default=".git")
     workdir = pathlib.Path(workdir)
     if workdir.is_file():
         raise Exception(f"{workdir} is not a directory")
-    os.makedirs(workdir / git_dir / "refs" / "heads", exist_ok=True)
-    os.makedirs(workdir / git_dir / "refs" / "tags", exist_ok=True)
+    os.makedirs(workdir / git_dir / "refs" / "heads")
+    os.makedirs(workdir / git_dir / "refs" / "tags")
     (workdir / git_dir / "objects").mkdir()
     with open(workdir / git_dir / "config", "w") as f:
         f.write("[core]\n\trepositoryformatversion = 0\n\tfilemode = true\n\tbare = false\n\tlogallrefupdates = false\n",)
