dbd1ebb3d19cb4f63df2175954404b422afaae5d
crawlic-stud <nikitosik0726@gmail.com>
2021-06-09 10:56:01 +0300

    database-done

--- homework06/db.py
+++ homework06/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+from scraputils import get_news
 
 Base = declarative_base()
 engine = create_engine("sqlite:///news.db")
@@ -19,7 +19,24 @@ class News(Base):
     points = Column(Integer)
     label = Column(String)
 
-
-
-
 Base.metadata.create_all(bind=engine)
+
+
+
+if __name__ == "__main__":
+    news_list = get_news("https://news.ycombinator.com/newest", n_pages=20)
+    length = len(news_list)
+    s = session()
+    for i in range(length):
+        news = News(
+            title=news_list[i]["title"],
+            author=news_list[i]["author"],
+            url=news_list[i]["link"],
+            comments=news_list[i]["comments"],
+            points=news_list[i]["points"],
+        )
+        s.add(news)
+        s.commit()
+
+
+
